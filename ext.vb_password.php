4e5a88144f68da4f031459ca2e6b0eca78bd3a81
Arron Woods <arron@copiadigital.co.uk>
2017-04-12 08:32:14 +0100

    Settings and db connection

--- ext.vb_password.php
+++ ext.vb_password.php
@@ -12,13 +12,13 @@
  */
 class Vb_password_ext
 {
-    public $version         = VB_PASSWORD_VERSION;
-    public $name            = VB_PASSWORD_NAME;
-    public $description     = 'Updates the password of a vbuser when changed in EE';
-    public $docs_url        = '';
-    public $settings_exist  = false;
-    public $settings        = array();
-    public $hooks           = array(
+    public $version = VB_PASSWORD_VERSION;
+    public $name = VB_PASSWORD_NAME;
+    public $description = 'Updates the password of a vbuser when changed in EE';
+    public $docs_url = '';
+    public $settings_exist = false;
+    public $settings = array();
+    public $hooks = array(
         'updateVbPassword' => 'after_member_save'
     );
 
@@ -28,39 +28,134 @@ class Vb_password_ext
      * @access public
      * @return void
      */
-    public function __construct()
-    { }
+    public function __construct($settings = '')
+    {
+        $this->settings = is_array($settings)? $settings : array();
+    }
 
-    public function updateVbPassword($member, $values){
+    /**
+     * @param $member
+     * @param $values
+     */
+    public function updateVbPassword($member, $values)
+    {
         $username = $member->username;
-	$password = $this->getPasswordFromPost();
+        $password = $this->getPasswordFromPost();
 
-        if(!empty($username) && !empty($password)){
+        if (!empty($username) && !empty($password)) {
+            try{
+                $this->updateVbUsersPassword($username, $password);
+            }catch(\Exception $e){
+                error_log($e);
+            }
+
             var_dump('update', $username, $password);
             exit;
         }
     }
 
-    public function getPasswordFromPost(){
+    /**
+     * @param $username
+     * @param $plainPassword
+     * @return bool|mysqli_result
+     */
+    public function updateVbUsersPassword($username, $plainPassword){
+        $salt = $this->getSalt();
+        $vbHash = $this->hashPasswordForVb($plainPassword);
+
+        $sql = "UPDATE `user` SET token = '$vbHash', scheme='legacy', secret='$salt' " ;
+        $sql.= "WHERE username LIKE '%[" . $username . "]%'";
+
+        $db = $this->getDbConnection();
+        return $db->query($sql);
+    }
+
+    /**
+     * @param $plainPassword
+     * @return string
+     */
+    public function hashPasswordForVb($plainPassword){
+        $salt = $this->getSalt();
+        $md5 = md5($plainPassword);
+        $hash = md5( $md5 . $salt) . " $salt";
+
+        return $hash;
+    }
+
+    /**
+     * @return mixed|null
+     */
+    public function getPasswordFromPost()
+    {
         $data = $_POST;
         return $this->searchArrayForPassword($data);
     }
 
-    protected function searchArrayForPassword(array $data){
-        foreach($data as $key => $value){
-	    if($key === 'new_password' && !empty($value)){
+    /**
+     * @return string
+     */
+    public function getSalt(){
+        return 'SWA';
+    }
+
+    /**
+     * @return mysqli
+     */
+    public function getDbConnection(){
+        $host = $this->settings['db_host'];
+        $user = $this->settings['db_user'];
+        $password = $this->settings['db_password'];
+        $dbname = $this->settings['db_name'];
+        $port = $this->settings['db_port'];
+
+        if(empty($port)){
+            $port = '3306';
+        }
+
+        $mysqli = new mysqli($host, $user, $password, $dbname, $port);
+        if ($mysqli->connect_error) {
+            throw new \RuntimeException('VB DB Connect Error (' . $mysqli->connect_errno . ') '
+                . $mysqli->connect_error);
+        }
+
+        return $mysqli;
+    }
+
+    /**
+     * @param array $data
+     * @return mixed|null
+     */
+    protected function searchArrayForPassword(array $data)
+    {
+        foreach ($data as $key => $value) {
+            if ($key === 'new_password' && !empty($value)) {
                 return $value;
             }
 
-            if(is_array($value)){
+            if (is_array($value)) {
                 $result = $this->searchArrayForPassword($value);
-                if($result !== null){
+                if ($result !== null) {
                     return $result;
                 }
             }
-	}
+        }
 
         return null;
+    }
+
+    /**
+     * @return array
+     */
+    public function settings(){
+        $settings = array();
+
+        $settings['db_host']      = array('i', '', "localhost");
+        $settings['db_user']      = array('i', '', "dbuser");
+        $settings['db_password']      = array('i', '', "");
+        $settings['db_name']      = array('i', '', "forum");
+        $settings['db_port']      = array('i', '', "3306");
+
+        return $settings;
     }
 
     /**
@@ -72,14 +167,14 @@ class Vb_password_ext
     public function activate_extension()
     {
         foreach ($this->hooks as $method => $hook) {
-             $data = array(
-                'class'     =>  __CLASS__,
-                'method'    =>  $method,
-                'hook'      =>  $hook,
-                'settings'  =>  serialize($this->settings),
-                'priority'  =>  100,
-                'version'   =>  $this->version,
-                'enabled'   =>  'y'
+            $data = array(
+                'class' => __CLASS__,
+                'method' => $method,
+                'hook' => $hook,
+                'settings' => serialize($this->settings),
+                'priority' => 100,
+                'version' => $this->version,
+                'enabled' => 'y'
             );
 
             // insert in database
@@ -106,7 +201,7 @@ class Vb_password_ext
      * @access public
      * @return void
      **/
-    public function update_extension($current=false)
+    public function update_extension($current = false)
     {
         if ($current == $this->version) return false;
 
@@ -123,13 +218,13 @@ class Vb_password_ext
             if (isset($dbexts[$hook]) === true) continue;
 
             $data = array(
-                'class'     =>  __CLASS__,
-                'method'    =>  $method,
-                'hook'      =>  $hook,
-                'settings'  =>  serialize($this->settings),
-                'priority'  =>  100,
-                'version'   =>  $this->version,
-                'enabled'   =>  'y'
+                'class' => __CLASS__,
+                'method' => $method,
+                'hook' => $hook,
+                'settings' => serialize($this->settings),
+                'priority' => 100,
+                'version' => $this->version,
+                'enabled' => 'y'
             );
 
             // insert in database
---
+++ language/english/vb_password_lang.php
@@ -0,0 +1,12 @@
+<?php if (!defined('BASEPATH')) die('No direct script access allowed');
+
+$lang = array(
+
+    'vb_password'     => 'VB Password',
+    'v:db_host'       => 'Database hostname',
+    'v:db_user'       => 'Database user',
+    'v:db_password'   => 'Database password',
+    'v:db_name'       => 'Database name',
+    'v:db_port'       => 'Database port',
+
+);
